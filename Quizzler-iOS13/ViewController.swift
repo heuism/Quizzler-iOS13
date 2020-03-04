015897b82cfca71255447f0562c11b765ecb088c
heuism <heuism23892@gmail.com>
2020-03-04 17:07:50 +1100

    Update the quiz with loop

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than One.", "True"],
+        ["Three + Eight is less than Ten.", "False"]
+    ]
+    
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionNumber = questionNumber % quiz.count
+        if questionNumber < quiz.count {
+            questionLabel.text = quiz[questionNumber][0]
+        }
+        
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        questionNumber += 1
+        updateUI()
+    }
+    
+    
 }
 
